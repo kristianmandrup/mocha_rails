c4aa036b7ab32485703284cb75b44071ea5fb72c
Matias Niemela <matias@yearofmoo.com>
2012-06-03 23:11:28 -0400

    updated the code; ready to test

--- app/controllers/mocha_rails/tests_controller.rb
+++ app/controllers/mocha_rails/tests_controller.rb
@@ -1,6 +1,45 @@
 module MochaRails
   class TestsController < ActionController::Base
+
+    layout 'mocha'
+
+    before_filter do
+      dir = Dir.exist?('spec') ? './spec' : './test'
+      dir += '/javascripts'
+      unless Dir.exist?(dir)
+        raise "mocha_rails: Testing directory (spec/javascripts or test/javascipts) does not exist"
+      end
+
+      @test_dir = dir
+    end
+
+
     def index
+      @mocha_suite = 'mocha-suite'
+    end
+
+    def list
+      match = '*-suite.js*'
+      search = File.join(@test_dir,match)
+      @mocha_suites = Dir[search]
+    end
+
+    def page
+      @mocha_suite = nil
+      suite = params[:suite].to_s
+      if suite.length == 0
+        redirect_to :action => :index
+        return
+      end
+
+      search = File.join(@test_dir,suite + '-suite.js*')
+      files = Dir[search]
+      if files.length > 0
+        @mocha_suite = files.first
+      end
+
+      render :action => :index
     end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 MochaRails::Engine.routes.draw do
-  root to: "tests#index"
+  root :to => "tests#index"
+  controller :test do
+    get "/list" => :list, :as => :mocha_tests_list
+    get "/:suite" => :page, :as => :mocha_test_page
+    get "/" => :index, :as => :mocha_tests_index
+  end
 end
