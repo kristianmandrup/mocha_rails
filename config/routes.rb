7f55e4651624ca187d44ef78421f2401a12a65f9
Matias Niemela <matias@yearofmoo.com>
2012-06-03 23:16:10 -0400

    updated the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 MochaRails::Engine.routes.draw do
   root :to => "tests#index"
-  controller :test do
+  controller :tests do
     get "/list" => :list, :as => :mocha_tests_list
     get "/:suite" => :page, :as => :mocha_test_page
     get "/" => :index, :as => :mocha_tests_index
